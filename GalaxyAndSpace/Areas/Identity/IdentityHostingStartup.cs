c2c2a20e1994610741c45311f4e1bdf2cec45676
miki <foxride345@gmail.com>
2020-07-16 17:03:08 +0300

    Initial commit

---
+++ GalaxyAndSpace/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using GalaxyAndSpace.Areas.Identity.Data;
+using GalaxyAndSpace.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(GalaxyAndSpace.Areas.Identity.IdentityHostingStartup))]
+namespace GalaxyAndSpace.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplectionUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                })
+
+                    .AddEntityFrameworkStores<AuthDbContext>();
+            });
+        }
+    }
+}
